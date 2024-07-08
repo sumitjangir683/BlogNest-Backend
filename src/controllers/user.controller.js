c33f9221c257d8d3c673e5206f8f737cfa98d114
sumitjangir683 <138011255+sumitjangir683@users.noreply.github.com>
2024-07-09 02:34:38 +0530

    Update user.controller.js

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -131,7 +131,8 @@ const loginUser = asyncHandler(async(req,res) => {
     const loggedInUser = await User.findById(user._id).select("-password -refreshToken")
   const options ={
     httpOnly:true,
-    secure:true
+    secure:true,
+       sameSite: 'None'
    }
    
   return res
