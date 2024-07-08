b65905812b5c1d2221daf00c67b5e1875271af3d
sumitjangir683 <138011255+sumitjangir683@users.noreply.github.com>
2024-07-09 00:09:51 +0530

    Update app.js

--- src/app.js
+++ src/app.js
@@ -3,13 +3,30 @@ import cors from "cors"
 import cookieParser from "cookie-parser"
 
 const app = express()
-const allowedOrigin =[process.env.CORS_ORIGIN,'http://localhost:5173']
-const corsOptions = {
-    origin: allowedOrigin, 
-    credentials: true, 
-  };
+// const allowedOrigin =[process.env.CORS_ORIGIN,'http://localhost:5173']
+// const corsOptions = {
+//     origin: allowedOrigin, 
+//     credentials: true, 
+//   };
   
-  app.use(cors(corsOptions));
+//   app.use(cors(corsOptions));
+const allowedOrigins = [
+ process.env.CORS_ORIGIN,
+  'http://localhost:5173',
+];
+
+app.use(
+  cors({
+    origin: (origin, callback) => {
+      if (!origin || allowedOrigins.indexOf(origin) !== -1) {
+        callback(null, true);
+      } else {
+        callback(new Error("Not allowed by CORS"));
+      }
+    },
+    credentials: true,
+  })
+);
 // app.use(cors({
 //     origin: process.env.CORS_ORIGIN,
 //     credentials: true
