3550b02a1fcc84fac9b3abe70f0a4105b42da89e
sumitjangir683 <138011255+sumitjangir683@users.noreply.github.com>
2024-07-09 15:11:21 +0530

    Update multer.middleware.js

--- src/midllewares/multer.middleware.js
+++ src/midllewares/multer.middleware.js
@@ -1,9 +1,16 @@
 import multer from "multer";
+const fs = require('fs');
+const path = require('path');
+
+const tempDir = path.join(__dirname, 'public', 'temp');
+if (!fs.existsSync(tempDir)) {
+  fs.mkdirSync(tempDir, { recursive: true });
+}
 const storage = multer.diskStorage({
     
     destination:function(req,file,cb){
         //console.log("multer");
-        cb(null,"./public/temp")
+        cb(null,tempDir)
     },
     filename: function(req,file,cb){
         cb(null,file.originalname)
